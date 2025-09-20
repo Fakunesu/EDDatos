99522b996cdb98c212672dd5dd68a1874eed2fc1
Fakunesu <facundo.minones1401@gmail.com>
2025-09-19 21:19:04 -0300

    a

--- Assets/Group 07/TP 02 Ejercicio 02/TP 02 Ejercicio 03/Scripts/PlayerInventory.cs
+++ Assets/Group 07/TP 02 Ejercicio 02/TP 02 Ejercicio 03/Scripts/PlayerInventory.cs
@@ -1,29 +1,105 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
-public class PlayerInventory
+public class PlayerInventory : MonoBehaviour
 {
     private DataBaseSO data;
+    [SerializeField] private StoreManager storeManager;
+    public int dineroJugador {private set; get; }
+
+    [Header("Panel")]
+    [SerializeField] private Transform itemContainer;
+
+    [Header("Prefab")]
+    [SerializeField] private GameObject itemButtonPrefab;
+    [SerializeField] private DataBaseSO itemVisuals;
+
+    [Header("Money")]
+    [SerializeField] private TextMeshProUGUI moneyText;
+    [SerializeField] private int dineroInicial = 10000;
 
     private Dictionary<int, int> items = new Dictionary<int, int>();
+    private Dictionary<int, GameObject> itemButton = new Dictionary<int, GameObject>();
 
     public int CantidadItems => items.Count;
 
-    public PlayerInventory(DataBaseSO data)
+
+
+    private void Awake()
     {
-        this.data = data;
+        dineroJugador = dineroInicial;
+        ActualizarDineroUI();
+        
+    }
+
+
+    public void VenderItem(ItemSO item)
+    {
+        if (!items.ContainsKey(item.ID))
+            return;
+        else
+        {
+            dineroJugador += item.Price;
+            ActualizarDineroUI();
+            EliminarItem(item);
+
+            if (storeManager != null)
+            {
+                storeManager.AgregarItem(item);
+                Debug.Log($"Vendiste: {item.ItemName}");
+            }
+            else 
+            {
+                
+            }
+        }
+    }
+
+    public void PlayerComproItem(ItemSO item)
+    {
+        dineroJugador -= item.Price;
+        AgregarItem(item);
+        ActualizarDineroUI();
     }
 
     public void AgregarItem(ItemSO item)
     {
         if (items.ContainsKey(item.ID))
+        {
             items[item.ID]++;
+            if (itemButton.ContainsKey(item.ID))
+            {
+               ActualizarTextoUI(item);
+            }
+        }
         else
+        {
             items.Add(item.ID, 1);
+            GameObject boton = Instantiate(itemButtonPrefab, itemContainer);
+            var texto = boton.GetComponentInChildren<TextMeshProUGUI>(true);
+            if (texto != null) texto.text = $"{item.ItemName} x1";
 
-        Debug.Log($"Inventario: {items.ToString()}");
+            var icon = boton.transform.Find("icon").GetComponent<Image>();
+            if (icon != null)
+            {
+                icon.sprite = item.Sprite;
+                icon.enabled = (item.Sprite != null);
+            }
+
+            Button btn = boton.GetComponent<Button>();
+            if (btn != null)
+            {
+                ItemSO itemCapturado = item;
+                btn.onClick.AddListener(() => VenderItem(itemCapturado));
+            }
+
+            itemButton.Add(item.ID, boton);
+
+        }
     }
-
     public void EliminarItem(ItemSO item)
     {
         if (items.ContainsKey(item.ID))
@@ -31,13 +107,41 @@ public class PlayerInventory
             items[item.ID]--;
 
             if (items[item.ID] < 1)
+            {
                 items.Remove(item.ID);
-            Debug.Log($"{item.ItemName} eliminado del inventario");
+
+                if (itemButton.ContainsKey(item.ID))
+                {
+                    Destroy(itemButton[item.ID]);
+                    itemButton.Remove(item.ID);    
+                }
+            }
+            else
+            {
+                ActualizarTextoUI(item);
+            }
+
+                Debug.Log($"{item.ItemName} eliminado del inventario");
         }
         else
         {
             Debug.Log($"{item.ItemName} no encontrado en el inventario");
         }
+
+    }
+
+    void ActualizarTextoUI(ItemSO item)
+    {
+      
+        
+            var texto = itemButton[item.ID].GetComponentInChildren<TextMeshProUGUI>(true);
+            if (texto != null) texto.text = $"{item.ItemName} x{items[item.ID]}";
+        
+    }
+    void ActualizarDineroUI()
+    {
+        if (moneyText != null)
+            moneyText.text = $"Money: ${dineroJugador}";
     }
 }
 
--- Assets/Group 07/TP 02 Ejercicio 02/TP 02 Ejercicio 03/Scripts/StoreManager.cs
+++ Assets/Group 07/TP 02 Ejercicio 02/TP 02 Ejercicio 03/Scripts/StoreManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEditor.Progress;
@@ -12,40 +13,33 @@ public class StoreManager : MonoBehaviour
     [SerializeField] private DataBaseSO itemVisuals;
     [SerializeField] private TextMeshProUGUI moneyText;
     [SerializeField] private DataBaseSO data;
+    [SerializeField] private PlayerInventory playerInventory;
     private Dictionary<int, int> items = new Dictionary<int, int>(); // ID, Cantidad
+    private Dictionary<int, GameObject> itemButton = new Dictionary<int, GameObject>();
 
-    [Header("Configuración")]
-    [SerializeField] private int dineroInicial = 10000;
-
-    //En vez de esto, tendria que ir la ItemListSO
-    //private ItemsDictionary itemsDB;
-    private PlayerInventory playerInventory;
-    private int dineroJugador;
-
-    private void Awake()
-    {
-        //for (int i = 0; i < data.items.Length; i++)
-        //    data.items[i].SetID(i);
-    }
 
     void Start()
     {
-        dineroJugador = dineroInicial;
-        ActualizarDineroUI();
 
-        //itemsDB = new ItemsDictionary(itemVisuals);
-        playerInventory = new PlayerInventory(data);
+        
 
-        //Reemplazar por ItemListSO y descomentar
+        Debug.Log($"{playerInventory.dineroJugador}");
+
 
         for (int i = 0; i < data.items.Length; i++)
         {
             ItemSO item = data.items[i];
             GameObject boton = Instantiate(itemButtonPrefab, itemContainer);
 
+
+            if (item.ID < 4)
+                items.Add(item.ID, 5); // Agregamos 5 de cada municion
+            else
+                items.Add(item.ID, 1); // Agregamos 1 de cada otros items
             // Texto
             var texto = boton.GetComponentInChildren<TextMeshProUGUI>(true);
-            if (texto != null) texto.text = $"{item.ItemName} - ${item.Price}";
+            items.TryGetValue(item.ID, out int amount);
+            if (texto != null) texto.text = $"{item.ItemName} \n x{ amount} \n -${item.Price}";
 
             // Imagen (en root o en un hijo)
             var icon = boton.transform.Find("icon").GetComponent<Image>();
@@ -55,26 +49,22 @@ public class StoreManager : MonoBehaviour
                 icon.enabled = (item.Sprite != null);
             }
 
+            itemButton.Add(item.ID, boton);
             ItemSO itemCapturado = item;
             Button btn = boton.GetComponent<Button>();
-            if (item.ID < 4)
-                items.Add(item.ID, 3); // Agregamos 3 de cada municion
-            else
-                items.Add(item.ID, 1); // Agregamos 3 de cada otros items
-
 
             if (btn != null)
-                btn.onClick.AddListener(() => ComprarItem(itemCapturado));
+                btn.onClick.AddListener(() => ComerciarItem(itemCapturado));
         }
     }
 
-    void ComprarItem(ItemSO item)
+    public void ComerciarItem(ItemSO item)
     {
-        if (dineroJugador >= item.Price)
+        Debug.Log($"el jugador tiene {playerInventory.dineroJugador}");
+        if (playerInventory.dineroJugador >= item.Price)
         {
-            dineroJugador -= item.Price;
-            ActualizarDineroUI();
-            playerInventory.AgregarItem(item);
+            playerInventory.PlayerComproItem(item);
+            EliminarItem(item);
             Debug.Log($"Inventario ahora: {playerInventory.CantidadItems} items.");
             Debug.Log($"You bought: {item.ItemName}");
         }
@@ -86,10 +76,73 @@ public class StoreManager : MonoBehaviour
 
 
 
-    void ActualizarDineroUI()
+    public void AgregarItem(ItemSO item)
     {
-        if (moneyText != null)
-            moneyText.text = $"Money: ${dineroJugador}";
+        if (items.ContainsKey(item.ID))
+        {
+            items[item.ID]++;
+            if (itemButton.ContainsKey(item.ID))
+            {
+                ActualizarTextoUI(item);
+            }
+        }
+        else
+        {
+            items.Add(item.ID, 1);
+            GameObject boton = Instantiate(itemButtonPrefab, itemContainer);
+            var texto = boton.GetComponentInChildren<TextMeshProUGUI>(true);
+            if (texto != null) texto.text = $"{item.ItemName} x1";
+
+            var icon = boton.transform.Find("icon").GetComponent<Image>();
+            if (icon != null)
+            {
+                icon.sprite = item.Sprite;
+                icon.enabled = (item.Sprite != null);
+            }
+
+            Button btn = boton.GetComponent<Button>();
+            if (btn != null)
+            {
+                ItemSO itemCapturado = item;
+                btn.onClick.AddListener(() => ComerciarItem(itemCapturado));
+            }
+
+            itemButton.Add(item.ID, boton);
+
+        }
     }
+
+    public void EliminarItem(ItemSO item)
+    {
+        if (items.ContainsKey(item.ID))
+        {
+            items[item.ID]--;
+            ActualizarTextoUI(item);
+            Debug.Log($"Cantidad de {item.ID} = {items[item.ID]}");
+            if (items[item.ID] < 1)
+            {
+                items.Remove(item.ID);
+                if (itemButton.ContainsKey(item.ID))
+                {
+                    Destroy(itemButton[item.ID]);
+                    itemButton.Remove(item.ID);
+                }
+            }
+            Debug.Log($"{items}");
+        }
+        else
+        {
+            Debug.Log($"{item.ItemName} no encontrado en el inventario");
+        }
+
+    }
+
+    void ActualizarTextoUI(ItemSO item)
+    {
+            var texto = itemButton[item.ID].GetComponentInChildren<TextMeshProUGUI>(true);
+            if (texto != null) texto.text = $"{item.ItemName} \n x{items[item.ID]} \n -${item.Price}"; 
+    }
+
+
 }
 
